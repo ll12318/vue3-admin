bdbd8a34b3dd9a281f1b616c1b71c4477ed11a53
wukang <w17681264762@163.com>
2023-05-05 16:31:24 +0800

    远程构建添加密码

--- src/views/layout/components/header.vue
+++ src/views/layout/components/header.vue
@@ -23,15 +23,23 @@
       ></i>
     </span>
     <div class="toolbar" style="float: right">
-        <el-tooltip
-                class="box-item"
-                effect="light"
-                content="Jenkins远程构建"
-                placement="left-start"
-        >
-            <el-button style="margin-right: 10px" type="text" @click="handle_jenkins"
-            ><i class="iconfont icon-send"></i></el-button>
-        </el-tooltip>
+        <el-popover :visible="visible" placement="bottom" :width="160">
+            <el-input
+                    placeholder="请输入内容"
+                    suffix-icon="el-icon-search"
+                    type="password"
+                    v-model="pwd"
+                    @keyup.enter.native="handle_jenkins"
+                    clearable
+                    size="mini"
+                    style="width: 100%" />
+            <template #reference>
+
+                    <el-button style="margin-right: 10px" type="text" @click="visible = true"
+                    ><i class="iconfont icon-send"></i></el-button>
+            </template>
+        </el-popover>
+
 
       <switch-theme></switch-theme>
 
@@ -44,6 +52,8 @@
 import { useLayoutStore } from "../../../store/layout";
 import { useThemeStore } from "../../../store/theme";
 import SwitchTheme from "./switch_theme.vue";
+import {ref} from "vue";
+import {ElNotification} from "element-plus";
 const layoutStore = useLayoutStore();
 const themeStore = useThemeStore();
 
@@ -54,8 +64,22 @@ const handleCollapse = (isCollapse: boolean) => {
   layoutStore.setLayout(isCollapse);
 };
 const handle_jenkins = () => {
+    if(pwd.value !== "liuli520"){
+        ElNotification({
+            message: "密码错误",
+            type: "error",
+            duration: 2000,
+        })
+        pwd.value = "";
+        visible.value = false;
+        return;
+    }
   window.open(
     "http://c.ll12138.icu:9000/job/vue3_admin/build?token=vue3_admin"
   );
+    pwd.value = "";
 };
+const visible = ref(false);
+const pwd = ref("");
+
 </script>
