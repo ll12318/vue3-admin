235766e63de6272a83f3b533c338c58663ec8824
wukang <w17681264762@163.com>
2023-05-05 09:45:08 +0800

    测试8

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { createPinia } from "pinia";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import "element-plus/theme-chalk/dark/css-vars.css";
-
+// @ts-ignore
+window.version = "1.0.0";
 const app = createApp(App);
 app.use(router);
 app.use(ElementPlus);
