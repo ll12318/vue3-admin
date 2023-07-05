8f6a6c46cb75ede74b73681ac02dbdd0b4231543
wk-win10-main <w17681264762@163.com>
2023-07-05 09:56:13 +0800

    wk-win10-main机器测试

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import "element-plus/dist/index.css";
 import "element-plus/theme-chalk/dark/css-vars.css";
 import "uno.css";
 // @ts-ignore
+// 123
 window.version = "1.0.0";
 const app = createApp(App);
 app.use(router);
